8f813e0da23c79e7dbd390d8e8e65f8e968eb718
Yedija Johanan Siregar <115084766+YedijaJS@users.noreply.github.com>
2025-12-07 17:48:24 +0700

    Implement login functionality with attempts counter
    Added a login loop that checks for username and password.

--- Yedija Johanan Siregar/tugas1.go
+++ Yedija Johanan Siregar/tugas1.go
@@ -3,13 +3,15 @@ package main
 import "fmt"
 
 func main() {
-  var n, j int
-  fmt.Scan(&n)
-
-  j = n
-  for j > 1 {
-    fmt.Print(j, " x ")
-  j = j - 1
+  var username, password string
+  count := 0
+  for {
+      fmt.Scan(&username, &password)
+      if username == "Admin" && password == "Admin" {
+    
+        break
+    }
+      count++
   }
-    fmt.Println(1)
+    fmt.Printf("%d percobaan gagal login", count)
 }
