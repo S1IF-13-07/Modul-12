5b8c014a56866b8667c8ff958ab4e70151c91e89
IsmailMarasabesy <faizismail749@gmail.com>
2025-12-09 00:17:50 +0700

    Add files via upload

---
+++ Ismail Marasabessy/soal3.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x, y int
+	fmt.Print("Masukkan x dan y: ")
+	fmt.Scan(&x, &y)
+	hasil := 0
+	for x >= y {
+		x = x - y
+		hasil++
+	}
+	fmt.Println(hasil)
+}
