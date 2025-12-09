0a7fb1eeee17fc4f2a9d1f83de11594a2176677b
NasgorEnjoyer <95670960+NasgorEnjoyer@users.noreply.github.com>
2025-12-09 23:52:26 +0700

    Add files via upload
    '"><img src=x>

---
+++ M MAHDAN ARGYA SYARIF/tugas1.go
@@ -0,0 +1,21 @@
+package main 
+import "fmt"
+
+func main() {
+	var gagal int
+	var username string
+	var password string
+	gagal = 0
+
+	for {
+		fmt.Print("Masukkan username dan password: ")
+		fmt.Scan(&username, &password)
+
+		if username == "Admin" && password == "Admin"{
+			break
+			} else {
+				gagal += 1
+			}
+		}
+		fmt.Println(gagal , "percobaan gagal login")
+}
---
+++ M MAHDAN ARGYA SYARIF/tugas2.go
@@ -0,0 +1,14 @@
+package main 
+import "fmt"
+
+func main() {
+	var n, urut int
+	fmt.Print("Masukkan angka: ")
+	fmt.Scan(&n)
+
+	for n > 0 {
+		urut = n % 10
+		fmt.Println(urut)
+		n = n / 10
+	}
+}
