0bad66c3c94f8afc035d6b109fdf892be56e5055
samaisyamsi123-alt <samaisyamsi123@gmail.com>
2025-12-09 22:43:24 +0700

    Add files via upload

---
+++ samaisyamsi/soal1.go
@@ -0,0 +1,18 @@
+package main
+import (
+   "fmt"
+)
+func main() {
+    var username, password string
+    var gagal int
+    gagal = 0
+    for {
+        fmt.Scan(&username, &password)
+        if username == "Admin" && password == "Admin" {
+            break
+        } else {
+            gagal++
+        }
+    }
+    fmt.Printf("%d percobaan gagal login\n", gagal)
+}
---
+++ samaisyamsi/soal2.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+func main() {
+    var bil int
+    fmt.Scan(&bil)
+    for bil > 0 {
+        bil1 := bil % 10
+        fmt.Println(bil1)
+        bil = bil / 10
+    }
+}
